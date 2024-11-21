d6106b07dcac0f3b1997ded6d55ae9e282b470f6
YanaMlnk <yanamelnyk1998@gmail.com>
2024-11-21 14:24:43 +0100

    javadoc comments for food package written

--- app/src/main/java/io/tamagotchi/food/Potato.java
+++ app/src/main/java/io/tamagotchi/food/Potato.java
@@ -1,11 +1,24 @@
 package io.tamagotchi.food;
 
+/**
+ * Represents a type of food called Potato in the Tamagotchi game.
+ */
 public class Potato extends Food {
+
+    /**
+     * Constructs a Potato object with a predefined price and health value.
+     * The image URL is set to "potato.png".
+     */
     public Potato() {
         super(1, 10);
         this.imageUrl = "potato.png";
     }
 
+    /**
+     * Returns a message specific to the Potato type of food.
+     *
+     * @return a message indicating the Potato is not very tasty, but it does the job.
+     */
     @Override
     String message() {
         return "Potato is not very tasty, but it does the job!";
