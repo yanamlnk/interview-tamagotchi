bb6f8dd62da89129cc0027a733954563ade945c2
YanaMlnk <yanamelnyk1998@gmail.com>
2024-11-21 14:50:20 +0100

    javadoc comments for questions are finished

--- app/src/main/java/io/tamagotchi/question/Questions.java
+++ app/src/main/java/io/tamagotchi/question/Questions.java
@@ -4,17 +4,41 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+/**
+ * Represents a collection of questions in the Tamagotchi game.
+ * Provides methods to retrieve questions and select random questions.
+ */
 public class Questions {
+
+    /**
+     * The list of questions.
+     */
     private List<Question> questions;
 
-    public Questions(List<Question> question) {
-        this.questions = question;
+    /**
+     * Constructs a new Questions object with the specified list of questions.
+     *
+     * @param questions the list of questions
+     */
+    public Questions(List<Question> questions) {
+        this.questions = questions;
     }
 
+    /**
+     * Returns the list of questions.
+     *
+     * @return the list of questions
+     */
     public List<Question> getQuestions() {
         return questions;
     }
 
+    /**
+     * Returns a list of 10 random questions from the collection.
+     * If there are fewer than 10 questions, returns all available questions.
+     *
+     * @return a list of 10 random questions
+     */
     public List<Question> getRandomQuestions() {
         List<Question> selectedQuestions = new ArrayList<>();
         Random random = new Random();
--- app/src/main/java/io/tamagotchi/question/QuestionsController.java
+++ app/src/main/java/io/tamagotchi/question/QuestionsController.java
@@ -8,8 +8,18 @@ import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.List;
 
+/**
+ * Controller class for managing questions in the Tamagotchi game.
+ * Provides methods to load questions from a file and generate file names based on language and mode.
+ */
 public class QuestionsController {
 
+    /**
+     * Loads questions from a JSON file specified by the file path.
+     *
+     * @param filePath the path to the JSON file containing the questions
+     * @return a Questions object containing the list of questions loaded from the file
+     */
     public Questions loadQuestionsFromFile(String filePath) {
         List<Question> questions = null;
         try (FileReader reader = new FileReader(filePath)) {
@@ -22,6 +32,13 @@ public class QuestionsController {
         return new Questions(questions);
     }
 
+    /**
+     * Generates a file name based on the specified language and mode.
+     *
+     * @param language the language of the questions
+     * @param mode the mode of the questions
+     * @return the generated file name
+     */
     public String generateFileName(String language, String mode) {
         return "/questions/" + language.toLowerCase() + "-" + mode.toLowerCase() + ".json";
     }
